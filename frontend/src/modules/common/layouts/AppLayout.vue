9b29eac650a539228ba19c559b655fc3845b3251
Juan Martinez <mcplux@outlook.com>
2026-02-11 15:39:49 -0500

    Frontend: Add a basic sidebar

--- frontend/src/modules/common/layouts/AppLayout.vue
+++ frontend/src/modules/common/layouts/AppLayout.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const sidebar = ref(false)
+
+const closeSidebar = () => {
+  sidebar.value = false
+}
+
+const toggleSidebar = () => {
+  sidebar.value = !sidebar.value
+}
+</script>
+
 <template>
-  <RouterView />
+  <div class="flex min-h-screen">
+    <aside
+      class="fixed lg:static bg-gray-600 w-64 lg:w-96 h-screen z-10 transform transition-transform rounded-r-lg flex flex-col"
+      :class="[sidebar ? 'translate-x-0' : '-translate-x-full']"
+    >
+      Sidebar
+    </aside>
+
+    <div
+      v-if="sidebar"
+      @click="closeSidebar"
+      class="fixed inset-0 bg-black/40 backdrop-blur-xs lg:hidden"
+    />
+
+    <main
+      class="w-full transition-all max-h-screen overflow-auto"
+      :class="[sidebar ? 'lg:ml-0' : 'lg:-ml-96']"
+    >
+      <button @click="toggleSidebar">Open Sidebar</button>
+      <RouterView />
+    </main>
+  </div>
 </template>
