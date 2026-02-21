420a25727bace8532dab95718c64f2a7cf71a40b
Juan Martinez <mcplux@outlook.com>
2026-02-20 19:56:03 -0500

    Improve approva layout

--- frontend/src/modules/common/layouts/AppLayout.vue
+++ frontend/src/modules/common/layouts/AppLayout.vue
@@ -1,46 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { Menu } from 'lucide-vue-next'
-
-const sidebar = ref(false)
-
-const closeSidebar = () => {
-  sidebar.value = false
-}
-
-const toggleSidebar = () => {
-  sidebar.value = !sidebar.value
-}
+import { ChevronDown } from 'lucide-vue-next'
 </script>
 
 <template>
-  <div class="flex min-h-screen">
-    <aside
-      class="fixed lg:static bg-blue-600 w-64 lg:w-96 h-screen z-10 transform transition-transform rounded-r-lg flex flex-col overflow-auto"
-      :class="[sidebar ? 'translate-x-0' : '-translate-x-full']"
-    >
-      Sidebar
-    </aside>
-
-    <div
-      v-if="sidebar"
-      @click="closeSidebar"
-      class="fixed inset-0 bg-black/40 backdrop-blur-xs lg:hidden"
-    />
-
-    <div
-      class="flex flex-col h-screen w-full p-5 overflow-auto transition-all"
-      :class="[sidebar ? 'lg:ml-0' : 'lg:-ml-96']"
-    >
-      <header>
-        <button @click="toggleSidebar">
-          <Menu class="size-8" />
-        </button>
+  <div class="flex flex-col h-screen">
+    <div class="bg-blue-600 p-5">
+      <header class="flex justify-between items-center container mx-auto">
+        <h1 class="text-white text-4xl font-bold">Approva</h1>
+        <div class="flex gap-2">
+          <ChevronDown class="size-8 text-white" />
+        </div>
       </header>
-
-      <main class="flex-1 max-w-4xl w-full mt-5 mx-auto">
-        <RouterView />
-      </main>
     </div>
+
+    <main class="flex-1 flex flex-col h-full overflow-auto">
+      <RouterView />
+    </main>
   </div>
 </template>
--- frontend/src/modules/requests/views/RequestsView.vue
+++ frontend/src/modules/requests/views/RequestsView.vue
@@ -1,53 +1,132 @@
 <script setup lang="ts">
-import { Pencil, Plus, Trash } from 'lucide-vue-next'
+import { Check, ChevronDown, ChevronLeft, ChevronRight, Clock, Eye, Plus, X } from 'lucide-vue-next'
 </script>
 
 <template>
-  <h1 class="text-4xl text-center font-bold uppercase">My Requests</h1>
-
-  <div class="flex justify-end mt-5">
-    <button
-      :class="[
-        'flex items-center justify-center gap-1',
-        'w-full md:w-auto px-3 py-2',
-        'border rounded text-white bg-blue-600',
-        'hover:bg-blue-700 cursor-pointer transition-colors',
-      ]"
-    >
-      <Plus class="size-4" />
-      Add request
-    </button>
-  </div>
-
-  <div class="grid grid-cols-1 md:grid-cols-2 mt-5">
-    <div class="border rounded p-2 space-y-2">
-      <p class="text-xs font-bold text-gray-700 text-right">#1</p>
-      <h2 class="text-2xl font-bold text-center">Title</h2>
-      <p class="text-center bg-gray-400 text-gray-900 font-bold rounded">Not reviewed</p>
-      <div class="flex gap-1">
+  <div class="bg-gray-300 p-5 border-b border-gray-400">
+    <div class="max-w-4xl mx-auto flex items-center gap-5">
+      <button class="flex items-center gap-1 bg-blue-600 text-white text-lg rounded py-2 px-6">
+        <Plus />
+        New Request
+      </button>
+      <div class="flex gap-2 flex-1">
         <button
-          :class="[
-            'flex items-center justify-center gap-1',
-            'w-1/2 p-1',
-            'border rounded text-white bg-blue-600',
-            'hover:bg-blue-700 cursor-pointer transition-colors',
-          ]"
+          class="flex justify-between items-center px-2 border border-slate-700 p-1 rounded w-1/2"
         >
-          <Pencil class="size-4" />
-          Edit
+          <p class="text-slate-700 font-bold">
+            Filter: <span class="text-black font-normal">All categories</span>
+          </p>
+          <ChevronDown />
         </button>
+
         <button
-          :class="[
-            'flex items-center justify-center gap-1',
-            'w-1/2 p-1',
-            'border rounded text-white bg-red-600',
-            'hover:bg-red-700 cursor-pointer transition-colors',
-          ]"
+          class="flex justify-between items-center px-2 border border-slate-700 p-1 rounded w-1/2"
         >
-          <Trash class="size-4" />
-          Delete
+          <p class="text-slate-700 font-bold">
+            Order: <span class="text-black font-normal">Most recent</span>
+          </p>
+          <ChevronDown />
         </button>
       </div>
+    </div>
+  </div>
+
+  <div class="max-w-4xl w-full mx-auto border-b border-gray-400 pt-5 px-5">
+    <button class="p-2 pr-4 text-lg font-semibold border-b border-blue-600">My requests</button>
+  </div>
+
+  <div
+    class="flex flex-col flex-1 gap-3 max-w-4xl w-full mx-auto p-5 border-b border-gray-400 overflow-auto"
+  >
+    <div class="flex gap-3 items-center rounded bg-gray-100 shadow p-5">
+      <div class="w-36">
+        <h3 class="text-lg font-bold text-slate-900 truncate">Vacations</h3>
+        <p class="text-slate-700 text-sm truncate">7 days free</p>
+      </div>
+
+      <div class="flex flex-1 gap-2 items-center px-5">
+        <div class="bg-amber-500 p-1 rounded-full">
+          <Clock class="text-white" />
+        </div>
+        <p class="text-amber-500 font-bold text-lg">Pending</p>
+      </div>
+
+      <ChevronDown />
+    </div>
+
+    <div class="flex gap-3 items-center rounded bg-gray-100 shadow p-5">
+      <div class="w-36">
+        <h3 class="text-lg font-bold text-slate-900 truncate">Free day</h3>
+        <p class="text-slate-700 text-sm truncate">Sickness</p>
+      </div>
+
+      <div class="flex flex-1 gap-2 items-center px-5">
+        <div class="bg-blue-500 p-1 rounded-full">
+          <Eye class="text-white" />
+        </div>
+        <p class="text-blue-500 font-bold text-lg">In review</p>
+      </div>
+
+      <ChevronDown />
+    </div>
+
+    <div class="flex gap-3 items-center rounded bg-gray-100 shadow p-5">
+      <div class="w-36">
+        <h3 class="text-lg font-bold text-slate-900 truncate">Purchase</h3>
+        <p class="text-slate-700 text-sm truncate">A new laptop for HR</p>
+      </div>
+
+      <div class="flex flex-1 gap-2 items-center px-5">
+        <div class="bg-green-500 p-1 rounded-full">
+          <Check class="text-white" />
+        </div>
+        <p class="text-green-500 font-bold text-lg">Approved</p>
+      </div>
+
+      <ChevronDown />
+    </div>
+
+    <div class="flex gap-3 items-center rounded bg-gray-100 shadow p-5">
+      <div class="w-36">
+        <h3 class="text-lg font-bold text-slate-900 truncate">Generic</h3>
+        <p class="text-slate-700 text-sm truncate">Hire Martin Garrix</p>
+      </div>
+
+      <div class="flex flex-1 gap-2 items-center px-5">
+        <div class="bg-red-500 p-1 rounded-full">
+          <X class="text-white" />
+        </div>
+        <p class="text-red-500 font-bold text-lg">Rejected</p>
+      </div>
+
+      <ChevronDown />
+    </div>
+  </div>
+
+  <div class="flex justify-end items-center gap-5 w-full max-w-4xl mx-auto p-5">
+    <p>Page 1 of 5</p>
+    <div class="flex items-center">
+      <button class="flex justify-center items-center h-9 w-14 border border-gray-400 rounded-l">
+        <ChevronLeft class="size-4" stroke-width="3" />
+      </button>
+      <button
+        class="flex justify-center items-center h-9 w-9 border border-gray-400 border-b-4 border-b-blue-600 text-sm font-bold"
+      >
+        1
+      </button>
+      <button
+        class="flex justify-center items-center h-9 w-9 border border-gray-400 text-sm font-bold"
+      >
+        2
+      </button>
+      <button
+        class="flex justify-center items-center h-9 w-9 border border-gray-400 text-sm font-bold"
+      >
+        3
+      </button>
+      <button class="flex justify-center items-center h-9 w-14 border border-gray-400 rounded-r">
+        <ChevronRight class="size-4" stroke-width="3" />
+      </button>
     </div>
   </div>
 </template>
